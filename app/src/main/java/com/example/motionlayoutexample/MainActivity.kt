6d32b7ee36dd129cd770fec96a119ed09e18e54a
pawanharariya <pawanharariya@gmail.com>
2023-09-21 23:53:31 +0530

    Add basic star animation using MotionLayout

--- app/src/main/java/com/example/motionlayoutexample/MainActivity.kt
+++ app/src/main/java/com/example/motionlayoutexample/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.motionlayoutexample
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var animationOneButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        animationOneButton = findViewById(R.id.animation_one_button)
+        animationOneButton.setOnClickListener {
+
+            val intent = Intent(applicationContext, AnimationOneActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
